a0df5897929129622f3c309d802f46e654c07b79
BUPPALA <uppala.bharath@oracle.com>
2017-02-21 01:00:44 +0530

    Added Cross Origin Resource sharing filter.

--- src/main/java/com/bits/saas/SaasDashboardApplication.java
+++ src/main/java/com/bits/saas/SaasDashboardApplication.java
@@ -3,6 +3,8 @@ package com.bits.saas;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import com.bits.saas.filter.CORSResponseFilter;
+
 /**
  * Created by Bharath Kumar Uppala on 10/02/2017.
  */
@@ -11,6 +13,7 @@ public class SaasDashboardApplication extends ResourceConfig {
     public SaasDashboardApplication() {
     	
     	super(JacksonFeature.class);
+    	register(CORSResponseFilter.class);
     	packages("com.bits.saas.providers");
 
     }
