1face6f56d9905fdbb2e3fd95a58181b74dfd830
BUPPALA <uppala.bharath@oracle.com>
2017-02-22 01:53:56 +0530

    Removed a syso Need to add log

--- src/main/java/com/bits/saas/filter/CORSResponseFilter.java
+++ src/main/java/com/bits/saas/filter/CORSResponseFilter.java
@@ -13,7 +13,6 @@ public class CORSResponseFilter implements ContainerResponseFilter {
 
 	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
 			throws IOException {
-		System.out.println("In filter");
 		MultivaluedMap<String, Object> headers = responseContext.getHeaders();
 		headers.add("Access-Control-Allow-Origin", "*");
 		headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
